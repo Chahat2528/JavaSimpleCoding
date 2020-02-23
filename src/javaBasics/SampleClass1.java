87a152e753d97995a4da36585e94081fd6770353
hsoni2010@gmail.com <hsoni2010@gmail.com>
2020-02-23 12:59:35 +0530

    syso update

--- src/javaBasics/SampleClass1.java
+++ src/javaBasics/SampleClass1.java
@@ -3,7 +3,7 @@ package javaBasics;
 public class SampleClass1 {
 	
 	public static void main(String[] args) {
-		System.out.println("First Entry on Java");
+		System.out.println("First Entry on Java Learning !");
 	}
 
 }
