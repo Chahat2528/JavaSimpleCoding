a806c3ee8a953d799ab1a96f6c735b70338582e6
Chahat2528 <113462811+Chahat2528@users.noreply.github.com>
2022-09-13 13:48:01 -0400

    Update
    Update the sampleclass1 with new print

--- src/javaBasics/SampleClass1.java
+++ src/javaBasics/SampleClass1.java
@@ -4,6 +4,7 @@ public class SampleClass1 {
 	
 	public static void main(String[] args) {
 		System.out.println("First Entry on Java Learning !");
+		System.out.println("Second Entry on Java Learning");
 	}
 
 }
